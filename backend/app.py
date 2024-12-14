497d3bfcd7fedcb79616b18b87af010265d9b3b4
RazSherf <101274250+RazSherf@users.noreply.github.com>
2024-12-14 16:51:37 +0200

    changed the return text

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask in Docker!"
+    return "Hello, Flask in Docker RAZ¸¸!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
