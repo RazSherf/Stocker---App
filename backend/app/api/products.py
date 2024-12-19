934caf2055c3ee05ac72fbb158c61e9313abe5ff
RazSherf <101274250+RazSherf@users.noreply.github.com>
2024-12-19 19:23:46 +0200

    testing

--- backend/app/api/products.py
+++ backend/app/api/products.py
@@ -4,7 +4,7 @@ products_bp = Blueprint('products', __name__)
 
 @products_bp.route('/api/products', methods=['GET'])
 def get_products():
-    return jsonify({"message": "Hello !!"})
+    return jsonify({"message": "Hello11 !!"})
 
 @products_bp.route('/api/products/<product_id>', methods=['GET'])
 def get_product(product_id):
